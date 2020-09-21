c79e5d6a829b60b832a9f47df4c39dc1f329e2d9
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2020-09-21 21:49:49 +0700

    init activity-1

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/mutiarahmatun/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/mutiarahmatun/helloworld/MainActivity.java
@@ -2,16 +2,7 @@ package id.ac.ui.cs.mobileprogramming.mutiarahmatun.helloworld;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Hello World");
     }
 
 }
