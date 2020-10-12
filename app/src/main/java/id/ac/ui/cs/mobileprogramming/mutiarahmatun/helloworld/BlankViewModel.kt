145733167b43d73dc2e3d6569808ee1ca4037e01
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2020-10-12 19:57:57 +0700

    add blankfragment with view model

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/mutiarahmatun/helloworld/BlankViewModel.kt
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.mutiarahmatun.helloworld
+
+import androidx.lifecycle.ViewModel
+
+class BlankViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
